d68182d6cfc277f65343d565b3dd413b16430fad
ngocban <daongocban1102@gmail.com>
2023-02-09 15:43:34 +0700

    post schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20220701234348) do
 
+    create_table "posts", force: :cascade do |t|
+        t.string "content"
+        t.integer "user_id"
+        t.datetime "created_at", null: false
+        t.datetime "updated_at", null: false
+        t.index ["user_id"], name: "index_posts_on_user_id"
+    end
 
     create_table "users", force: :cascade do |t|
       t.string "email", default: "", null: false
