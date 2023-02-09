e8fb7a23e6b2b7c125c433b0e3af2221e1f6f8c5
ngocban <daongocban1102@gmail.com>
2023-02-09 15:53:49 +0700

    create photo schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20220701234348) do
+    create_table "photos", force: :cascade do |t|
+        t.string "image"
+        t.integer "post_id"
+        t.datetime "created_at", null: false
+        t.datetime "updated_at", null: false
+        t.index ["post_id"], name: "index_photos_on_post_id"
+    end
 
     create_table "posts", force: :cascade do |t|
         t.string "content"
